6f826d5a33664e4bd5f8e9d52c89772403762309
yogeshdhangar5213 <yogeshdhangar5213@gmail.com>
2022-11-12 08:42:15 -0800

    Added:Calculate For Premium Ride

--- src/main/java/CabInVoiceGenerator.java
+++ src/main/java/CabInVoiceGenerator.java
@@ -2,55 +2,79 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 public class CabInVoiceGenerator {
-    public static final int COST_PER_KMS = 10;
-    public static final int COST_PER_MIN = 1;
+    public static int costPerKms=10;
+    public static final int COST_PER_MIN=1;
+    public static int totalNumberOfRides;
+    public static double totalFare;
+    public static double AverageFarePerRide;
+    public static double totalDistance;
+    public static double totalTime;
 
-    public static double getInvoice(double totalKms, double getTime) {
+    static List<RideRepository> userList = new ArrayList<>();
 
+
+
+    static double generateInvoiceAsPerUserID(int id) {
+        RideRepository user1 = new RideRepository(1,10.0,20.0);
+        RideRepository user2 = new RideRepository(2,20.0,40.0);
+        RideRepository user3 = new RideRepository(3,30.0,60.0);
+        userList.add(user1);
+        userList.add(user2);
+        userList.add(user3);
+
+        return getInvoice(userList.get(id).getTotalDistance(),userList.get(id).getTotalRideDuration(),"R");
+    }
+
+
+    public static void getEnhancedInvoice() {
+        int counter = 1;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rides");
+        totalNumberOfRides = sc.nextInt();
+        while(counter<=totalNumberOfRides){
+            System.out.println("Enter the details of "+counter+" ride");
+            System.out.println("Enter the distance for ride: "+counter);
+            double distanceCovered = sc.nextDouble();
+            totalDistance = totalDistance + distanceCovered;
+            System.out.println("Enter the time for ride: "+counter);
+            double timeSpent = sc.nextDouble();
+            totalTime = totalTime + timeSpent;
+            counter++;
+        }
+        getInvoice(totalDistance,totalTime,"R");
+
+        sc.close();
+    }
+
+    public static double getInvoice(double totalKms,double getTime,String PremiumOrRegular) {
         double totalFare;
-        totalFare = (totalKms * COST_PER_KMS) + (getTime * COST_PER_MIN);
-        if (totalFare <= 5) {
-            totalFare = 5;
+        if(PremiumOrRegular.equals("P")) {
+            costPerKms=20;
+        }
+        totalFare = (totalKms*costPerKms)+(getTime*COST_PER_MIN);
+        if(totalFare<=5) {
+            totalFare=5;
             System.out.println("--------------Invoice------------");
             System.out.println("Total payable amount : 5");
             System.out.println("---------------------------------");
             return totalFare;
 
-        } else {
+        }else {
             System.out.println("--------------Invoice------------");
-            System.out.println("Total distance travelled : " + totalKms);
-            System.out.println("Ride Duration : " + getTime);
-            System.out.println("Total payable amount : " + totalFare);
+            System.out.println("Total distance travelled : "+totalKms);
+            System.out.println("Total Ride Duration : "+getTime);
+            System.out.println("Total payable amount for ride is "+totalFare);
+            System.out.println("Average Fare Per Ride is "+totalFare/totalNumberOfRides);
+
             System.out.println("---------------------------------");
             return totalFare;
         }
     }
 
-    public static double totalPayableAmount(double totalCabCharge[]) {
-        double sum = 0;
-        for (int i = 0; i < totalCabCharge.length; i++) {
-            sum = sum + totalCabCharge[i];
-        }
-        return sum;
-    }
-
     public static void main(String[] args) {
-        int counter = 1;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number of rides");
-        int numberOfRides = sc.nextInt();
-        double totalCabCharge[] = new double[numberOfRides];
-        while (counter <= numberOfRides) {
-            System.out.println("Enter the details of " + counter + " ride");
-            System.out.println("Enter the distance for ride: " + counter);
-            double distanceCovered = sc.nextDouble();
-            System.out.println("Enter the time for ride: " + counter);
-            double timeSpent = sc.nextDouble();
-            totalCabCharge[counter - 1] = getInvoice(distanceCovered, timeSpent);
-            counter++;
-        }
-        //double sum =  totalPayableAmount(totalCabCharge);
-        System.out.println("Total payable Amount for " + numberOfRides + " ride is " + totalPayableAmount(totalCabCharge));
+        getInvoice(10.0, 20.0, "P");
+//		getEnhancedInvoice();
+    //	generateInvoiceAsPerUserID();
+        generateInvoiceAsPerUserID(5);
     }
 }
-
